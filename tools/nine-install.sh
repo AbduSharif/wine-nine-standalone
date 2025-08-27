36474613557527e40d4ce328be55290022bb9c88
AbduSharif <57298920+AbduSharif@users.noreply.github.com>
2025-08-27 15:07:24 +0200

    Almost forgot to do this

--- tools/nine-install.sh
+++ tools/nine-install.sh
@@ -15,7 +15,7 @@ ln -sf "$BASE/lib64/d3d9-nine.dll.so" "$DST/d3d9-nine.dll"
 ln -sf "$BASE/bin64/ninewinecfg.exe.so" "$DST/ninewinecfg.exe"
 
 echo "enabling gallium nine"
-wine64 ninewinecfg.exe -e
+wine ninewinecfg.exe -e
 
 echo "done"
 exit 0
